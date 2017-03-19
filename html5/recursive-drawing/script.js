3d8eb4a181d358b244a6e2e1af37537bdcf44d43
John <t.malmberg190@gmail.com>
2017-03-19 22:54:54 +0100

    Added some color

--- html5/recursive-drawing/script.js
+++ html5/recursive-drawing/script.js
@@ -16,16 +16,17 @@ var l =
 	y2: 0,
 	l:  40,
 	s:  12,
-	d:  20
+	d:  20,
+	itr:12
 };
 
-function line(x1, y1, x2, y2, s)
+function line(x1, y1, x2, y2, s, r, g, b, a)
 {
 	ctx.beginPath();
 	ctx.moveTo(x1 + (canvas.width / 2), y1 + (canvas.height / 2));
 	ctx.lineTo(x2 + (canvas.width / 2), y2 + (canvas.height / 2));
 	ctx.lineWidth = s;
-	ctx.strokeStyle = 'black';
+	ctx.strokeStyle = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a, ')';
 	ctx.stroke();
 };
 
@@ -45,7 +46,7 @@ function render(x, y, i, a, s, itr, k)
 			l.x2 = l.x1 - (k * 3 * Math.cos((-135 + a) * PI2RAD));
 			l.y2 = l.y1 - (k * 3 * Math.sin((-135 + a) * PI2RAD));
 
-			line(l.x1, l.y1, l.x2, l.y2, s);
+			line(l.x1, l.y1, l.x2, l.y2, s, 0, 0, 0, 1);
 		}
 
 		l.x1 = x;
@@ -53,16 +54,16 @@ function render(x, y, i, a, s, itr, k)
 		l.x2 = l.x1 - (k * Math.cos((90 + a) * PI2RAD));
 		l.y2 = l.y1 - (k * Math.sin((90 + a) * PI2RAD));
 
-		line(l.x1, l.y1, l.x2, l.y2, s);
-		setTimeout(render, 25, l.x2, l.y2, i, l.d + a, s, itr, k - i);
+		line(l.x1, l.y1, l.x2, l.y2, s, (l.itr * 6) - (i * 10), (l.itr * 8) - (i * 10), 0, (12 / 100) * i);
+		setTimeout(render, 250, l.x2, l.y2, i, l.d + a, s, itr, k - i);
 
 		l.x1 = x;
 		l.y1 = y;
 		l.x2 = l.x1 - (k * Math.cos(a * PI2RAD));
 		l.y2 = l.y1 - (k * Math.sin(a * PI2RAD));
 
-		line(l.x1, l.y1, l.x2, l.y2, s);
-		setTimeout(render, 25, l.x2, l.y2, i, -l.d + a, s, itr, k - i);
+		line(l.x1, l.y1, l.x2, l.y2, s, (l.itr * 6) - (i * 10), (l.itr * 8) - (i * 10), 0, (12 / 100) * i);
+		setTimeout(render, 250, l.x2, l.y2, i, -l.d + a, s, itr, k - i);
 	}
 };
 
@@ -70,11 +71,8 @@ function main()
 {
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	var iterations = 12;
-	l.l = iterations * 7;
-
-	render(l.x2, l.y2, iterations, 45, l.s, iterations - 1, l.l);
+	l.l = l.itr * 7;
+	render(l.x2, l.y2, l.itr, 45, l.s, l.itr - 1, l.l);
 };
 
 main();
